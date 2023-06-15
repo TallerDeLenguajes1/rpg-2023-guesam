3bf4931b2e914048afa0d4da34807de269341781
LucianoBonilla <lucianobonilla27@gmail.com>
2023-06-15 17:47:26 -0300

    personajes

--- clases.cs
+++ clases.cs
@@ -4,14 +4,22 @@ public class personaje {
     private string? tipo;
     private string? nombre;
     private string? apellido;
+    private DateTime fechaNacimiento;
     private int edad;
+    private int velocidad;
     private int destreza;
     private int fuerza;
-    private int velocidad;
     private int armadura;
+    private int salud;
 
     public string? Tipo { get => tipo; set => tipo = value; }
     public string? Nombre { get => nombre; set => nombre = value; }
     public string? Apellido { get => apellido; set => apellido = value; }
+    public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
     public int Edad { get => edad; set => edad = value; }
+    public int Velocidad { get => velocidad; set => velocidad = value; }
+    public int Destreza { get => destreza; set => destreza = value; }
+    public int Fuerza { get => fuerza; set => fuerza = value; }
+    public int Armadura { get => armadura; set => armadura = value; }
+    public int Salud { get => salud; set => salud = value; }
 }
